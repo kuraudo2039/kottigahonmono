f91f5b5d9b26076912a9dbd796ef896cc539a175
Tatsuya Nakajima <kuraudo.2039@gmail.com>
2018-07-17 11:37:01 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+$(function(){
+  });
+/*
+  $('.tab-1').click(function(){
+    $('.type').css('display','none');
+    $('#mechanical').css('display','block');
+
+  });
+
+  $('.tab-2').click(function(){
+    $('.type').css('display','none');
+    $('#electronical').css('display','block');
+  });
+
+  $('.tab-3').click(function(){
+    $('.type').css('display','none');
+    $('#chemical').css('display','block');
+  });
+
+  $('.tab-4').click(function(){
+    $('.type').css('display','none');
+    $('#zivil').css('display','block');
+  });
+*/
+
+//ここからcondition.htmlの編集
+
+});
